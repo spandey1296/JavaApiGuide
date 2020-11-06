0c32db6a3e7678aae71def13b09c9a3a5057cbda
shivant47@gmail.com <shivant47@gmail.com>
2020-11-06 22:22:02 +0530

    url tested

--- src/com/company/UrlDemo.java
+++ src/com/company/UrlDemo.java
@@ -12,6 +12,13 @@ public class UrlDemo {
             System.out.println("Host Name: "+url.getHost());
             System.out.println("Port Number: "+url.getPort());
             System.out.println("File Name: "+url.getFile());
+            System.out.println("Protocol: "+url.getProtocol());
+            System.out.println("Host Name: "+url.getHost());
+            System.out.println("Port Number: "+url.getPort());
+            System.out.println("Default Port Number: "+url.getDefaultPort());
+            System.out.println("Query String: "+url.getQuery());
+            System.out.println("Path: "+url.getPath());
+            System.out.println("File: "+url.getFile());
 
         }catch(Exception e){
             System.out.println(e);
