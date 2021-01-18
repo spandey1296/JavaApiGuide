e2d127a4a632e2e05a80cb20611195bb74d36e37
shivant47@gmail.com <shivant47@gmail.com>
2021-01-18 23:00:46 +0530

    Binary Search using Iteration

---
+++ src/com/learn/datastructure/BinarySearchIterative.java
@@ -0,0 +1,30 @@
+package com.learn.datastructure;
+
+public class BinarySearchIterative {
+    public static void main(String[] args) {
+
+        int[] array = {1, 4, 7, 9, 16, 56, 70};
+        int n = 7;
+        int element = 9;
+        int found_index = recursiveBinarySearch(array, 0, n - 1, element);
+        if (found_index == -1) {
+            System.out.println("Element not found in the array ");
+        } else {
+            System.out.println("Element found at index:"+ found_index);
+        }
+
+
+    }
+
+    public static int recursiveBinarySearch(int[] array, int start_index, int end_index, int element) {
+        if (end_index >= start_index) {
+            int middle = start_index + (end_index - start_index) / 2;
+            if (array[middle] == element)
+                return middle;
+            if (array[middle] > element)
+                return recursiveBinarySearch(array, start_index, middle - 1, element);
+            return recursiveBinarySearch(array, middle + 1, end_index, element);
+        }
+        return -1;
+    }
+}
